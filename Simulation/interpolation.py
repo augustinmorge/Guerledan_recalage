210d24869f87ff13b7286676523fc9c0af125038
Florian Jerram <florian.jerram@ensta-bretagne.org>
2022-12-09 10:45:46 +0100

    interpolation.py

--- Simulation/interpolation.py
+++ Simulation/interpolation.py
@@ -4,7 +4,26 @@ import numpy as np
 
 def interpolation(lat, lon, mnt):
 	lat_mnt = mnt[:,0]
-	lon_mnt = mtn[:,1]
+	lon_mnt = mnt[:,1]
+	points = np.vstack((lat_mnt, lon_mnt)).T
+
+	point = np.array([lat, lon])
+	distances = np.linalg.norm(points - point, axis=1)
+	i_pos = np.argmin(distances)
+
+	if lat_mnt[i_pos]-lat<=0 and lon_mnt[i_pos]-lon<=0:
+		return (mnt[i_pos][2] + mnt[i_pos+1][2])/2
+
+	elif lat_mnt[i_pos]-lat>=0 and lon_mnt[i_pos]-lon>=0:
+		return (mnt[i_pos-1][2] + mnt[i_pos][2])/2
+
+	else: 
+		return mnt[i_pos][2]
+
+def interpolation2(lat, lon, mnt):
+	lat_mnt = mnt[:,0]
+	lon_mnt = mnt[:,1]
+	points = np.vstack((lat_mnt, lon_mnt)).T
 
 	min_dist = 1e5
 	for i in range(len(lat_mnt)):
@@ -13,16 +32,23 @@ def interpolation(lat, lon, mnt):
 			i_pos = i
 
 	if lat_mnt[i_pos]-lat<=0 and lon_mnt[i_pos]-lon<=0:
-		return (mnt[i_pos, 3] + mnt[i_pos+1, 3])/2
+		return (mnt[i_pos][2] + mnt[i_pos+1][2])/2
 
 	elif lat_mnt[i_pos]-lat>=0 and lon_mnt[i_pos]-lon>=0:
-		return (mnt[i_pos-1, 3] + mnt[i_pos, 3])/2
+		return (mnt[i_pos-1][2] + mnt[i_pos][2])/2
 
 	else: 
-		return mnt[i_pos, 3]
+		return mnt[i_pos][2]
 
 
 
 if __name__ == "__main__":
-	# interpolation()
-	print("J'interpole mamène")
+	AimeNT = np.loadtxt("./resources/guerledan_EDF_2013-06_MNT1m.tiff.txt", dtype = str)
+	MNT = []
+	for i in AimeNT:
+		MNT.append(i.split(','))
+		MNT[-1] = [np.float64(MNT[-1][0]), np.float64(MNT[-1][1]), np.float64(MNT[-1][2]+'.'+MNT[-1][3])] 
+	MNT = np.array(MNT)
+	print(interpolation(48.1989495, -3.0148023, MNT))
+	# print(interpolation2(48.1989495, -3.0148023, MNT))
+
